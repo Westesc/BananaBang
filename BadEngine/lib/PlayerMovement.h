66063121b2d89982f4b6d7c6e2c0a8f7043ced8b
ames123 <126474987+ames123@users.noreply.github.com>
2024-06-06 16:09:27 +0200

    GameObject player

--- BadEngine/lib/PlayerMovement.h
+++ BadEngine/lib/PlayerMovement.h
@@ -25,6 +25,7 @@ private:
     Camera* camera;
     TimeManager* tm;
     RigidBody* rb;
+    GameObject* player;
 
     //animation
     std::queue < std:: string > queueAnim;
@@ -72,34 +73,34 @@ private:
                 rotateAngle += direction * rotationAmount;
             }
             float angle = atan2(camera->getFront().x, camera->getFront().z) * (180.0 / M_PI);
-            sm->getActiveScene()->findByName("player")->getTransform()->localRotation.y = angle + rotateAngle;
+            player->getTransform()->localRotation.y = angle + rotateAngle;
         }
     }
     void move(float deltaTime, bool isRotate = true) {
         getRotate(isRotate);
 
-        float rotate = sm->getActiveScene()->findByName("player")->getTransform()->getLocalRotation().y;
+        float rotate = player->getTransform()->getLocalRotation().y;
         float sinRotate = sin(glm::radians(rotate));
         float cosRotate = cos(glm::radians(rotate));
 
-        float dx = sm->getActiveScene()->findByName("player")->getTransform()->getLocalScale().x * sm->getActiveScene()->findByName("player")->getAnimateBody()->getPosition().z * sinRotate + sm->getActiveScene()->findByName("player")->getAnimateBody()->getPosition().x * cosRotate;
-        float dz = sm->getActiveScene()->findByName("player")->getTransform()->getLocalScale().z * sm->getActiveScene()->findByName("player")->getAnimateBody()->getPosition().z * cosRotate - sm->getActiveScene()->findByName("player")->getAnimateBody()->getPosition().x * sinRotate;
+        float dx = player->getTransform()->getLocalScale().x * player->getAnimateBody()->getPosition().z * sinRotate + player->getAnimateBody()->getPosition().x * cosRotate;
+        float dz = player->getTransform()->getLocalScale().z * player->getAnimateBody()->getPosition().z * cosRotate - player->getAnimateBody()->getPosition().x * sinRotate;
 
        // sm->getActiveScene()->findByName("player")->Move(glm::vec3(0.f, dy, 0.f));
         glm::vec3 vel = glm::vec3(dx, 0.f, dz)/ deltaTime;
-        sm->getActiveScene()->findByName("player")->velocity = vel * 4.0f;
+        player->velocity = vel * 4.0f;
     }
 
     void moveInAir(float speed, float deltaTime) {
         getRotate(true);
         float angle = atan2(camera->getFront().x, camera->getFront().z) * (180.0 / M_PI);
-         sm->getActiveScene()->findByName("player")->getTransform()->localRotation.y = angle + rotateAngle;
+        player->getTransform()->localRotation.y = angle + rotateAngle;
         float distance = speed * tm->getFramePerSeconds();
-        float dx = distance * sin(glm::radians(sm->getActiveScene()->findByName("player")->getTransform()->getLocalRotation().y));
-        float dz = distance * cos(glm::radians(sm->getActiveScene()->findByName("player")->getTransform()->getLocalRotation().y));
+        float dx = distance * sin(glm::radians(player->getTransform()->getLocalRotation().y));
+        float dz = distance * cos(glm::radians(player->getTransform()->getLocalRotation().y));
         std::cout<< "dx: " << dx / deltaTime << "dz: " << dz / deltaTime << std::endl;
-        sm->getActiveScene()->findByName("player")->velocity.x = dx / deltaTime;
-        sm->getActiveScene()->findByName("player")->velocity.z = dz / deltaTime;
+        player->velocity.x = dx / deltaTime;
+        player->velocity.z = dz / deltaTime;
     }
 
     void MovePlayer(float deltaTime) {
@@ -112,7 +113,7 @@ private:
             if (input->checkKey(GLFW_KEY_W)) {
                 speed = airSpeed;
                 if (state == PlayerState::walking) {
-                    sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimation("walking");
+                    player->getAnimateBody()->setActiveAnimation("walking");
                 }
                 currentTurn = 180.f;
                 if (input->checkKey(GLFW_KEY_A)) currentTurn = -135.f;
@@ -121,7 +122,7 @@ private:
             else if (input->checkKey(GLFW_KEY_S)) {
                 speed = airSpeed;
                 if (state == PlayerState::walking) {
-                    sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimation("walking");
+                    player->getAnimateBody()->setActiveAnimation("walking");
                 }
                 currentTurn = 0.f;
                 if (input->checkKey(GLFW_KEY_A)) currentTurn = -45.f;
@@ -130,14 +131,14 @@ private:
             else if (input->checkKey(GLFW_KEY_D)) {
                 speed = airSpeed;
                 if (state == PlayerState::walking) {
-                    sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimation("walking");
+                    player->getAnimateBody()->setActiveAnimation("walking");
                 }
                 currentTurn = 90;
             }
             else if (input->checkKey(GLFW_KEY_A)) {
                 speed = airSpeed;
                 if (state == PlayerState::walking) {
-                    sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimation("walking");
+                    player->getAnimateBody()->setActiveAnimation("walking");
                 }
                 currentTurn = -90;
             }
@@ -158,7 +159,7 @@ private:
         rb->useGravity();
         MovePlayer(deltaTime);
 
-        glm::vec3 finalPosition = sm->getActiveScene()->findByName("player")->getTransform()->getLocalPosition();
+        glm::vec3 finalPosition = player->getTransform()->getLocalPosition();
         float jumpDistance = glm::length(finalPosition.y - initialPosition.y);
         /*if (jumpDistance == 0.0f) {
             state = PlayerState::walking;
@@ -168,7 +169,7 @@ private:
         if (jumpDistance > limitJump) {
             state = PlayerState::air;
             rb->upwardsSpeed = 0.f;
-            sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimationWithY("jumping down", true);
+            player->getAnimateBody()->setActiveAnimationWithY("jumping down", true);
         }
     }
 
@@ -178,7 +179,7 @@ private:
         }
         if (input->checkAnyKey()) {
              if (state == PlayerState::climbing) {
-                 initialPosition.y = sm->getActiveScene()->findByName("player")->getTransform()->getLocalPosition().y;
+                 player->getTransform()->getLocalPosition().y;
                  if (input->checkKey(GLFW_KEY_W)) {
                      currentClimbingSpeed = climbingSpeed;
                  }
@@ -200,17 +201,17 @@ private:
                         queueAnim.push("attack" + queueAnim.size() + 1);
                     }
                     state = PlayerState::attack1;
-                    sm->getActiveScene()->findByName("player")->children.at(0)->active = true;
-                    sm->getActiveScene()->findByName("player")->children.at(0)->getTransform()->localPosition = sm->getActiveScene()->findByName("player")->getTransform()->localPosition;
+                    player->children.at(0)->active = true;
+                    player->children.at(0)->getTransform()->localPosition = player->getTransform()->localPosition;
                 }
                 if (input->checkKey(GLFW_MOUSE_BUTTON_RIGHT))
                 {
                     state = PlayerState::dodge;
                 }
                 else if (input->checkKey(GLFW_KEY_SPACE) && !wasSpacePressed) {
-                    sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimationWithY("jumping up", true);
+                    player->getAnimateBody()->setActiveAnimationWithY("jumping up", true);
                     state = PlayerState::jump_up;
-                    initialPosition = sm->getActiveScene()->findByName("player")->getTransform()->getLocalPosition();
+                    initialPosition = player->getTransform()->getLocalPosition();
                 }
             }
             else {
@@ -218,19 +219,19 @@ private:
             }
         }
         else if (state == PlayerState::walking) {
-            sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimation("standing");
+            player->getAnimateBody()->setActiveAnimation("standing");
         }
         else {
             currentClimbingSpeed = 0.1f;
         }
-        if (glm::distance(sm->getActiveScene()->findByName("player")->getTransform()->getLocalPosition().y, groundPosition) < 0.5f && state == PlayerState::air) {
+        if (glm::distance(player->getTransform()->getLocalPosition().y, groundPosition) < 0.5f && state == PlayerState::air) {
             state = PlayerState::walking;
-            sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimation("standing");
-            sm->getActiveScene()->findByName("player")->getTransform()->localPosition.y = initialPosition.y;
+            player->getAnimateBody()->setActiveAnimation("standing");
+            player->getTransform()->localPosition.y = initialPosition.y;
         }
         wasSpacePressed = input->checkKey(GLFW_KEY_SPACE);
         if (state != PlayerState::attack1) {
-			sm->getActiveScene()->findByName("player")->children.at(0)->active = false;
+            player->children.at(0)->active = false;
         }
     }
 
@@ -242,9 +243,11 @@ public:
         this->camera = camera;
         this->tm = tm;
         rb = new RigidBody("player", sm, tm);
+        player = sm->getActiveScene()->findByName("player");  
     }
 
     void ManagePlayer(float& deltaTime2, float deltaTime) {
+        player = sm->getActiveScene()->findByName("player");
         //std::cout << sm->getActiveScene()->findByName("player")->getTransform()->localPosition.y << std::endl;
         this->deltaTime2 = deltaTime2;
         PlayerState prevState = state;
@@ -256,12 +259,12 @@ public:
         else if (state == PlayerState::attack1) {
             if (animCount == 0 && queueAnim.size()!= 0) {
                 animCount++;
-                sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimation("attack1");
+                player->getAnimateBody()->setActiveAnimation("attack1");
             }
             move(deltaTime, false);
-            if (sm->getActiveScene()->findByName("player")->getAnimateBody()->isPlay() == false) {
+            if (player->getAnimateBody()->isPlay() == false) {
                 if (queueAnim.size() > 1 && animCount == 1) {
-                    sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimation("attack2");
+                    player->getAnimateBody()->setActiveAnimation("attack2");
                     animCount++;
                 }
                 else if (queueAnim.size() > 2 && animCount == 2) {
@@ -269,7 +272,7 @@ public:
                         while (!queueAnim.empty()) {
                             queueAnim.pop();
                         }
-                        sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimation("attack3");
+                        player->getAnimateBody()->setActiveAnimation("attack3");
                 }
                 else {
                     state = PlayerState::walking;
@@ -277,38 +280,38 @@ public:
             }
         }
         else if (state == PlayerState::dodge) {
-            sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimation("dodge");
+            player->getAnimateBody()->setActiveAnimation("dodge");
             //currentTurn = 180.f;
             move(deltaTime, false);
-            if (sm->getActiveScene()->findByName("player")->getAnimateBody()->isPlay() == false) {
+            if (player->getAnimateBody()->isPlay() == false) {
                 state = PlayerState::walking;
             }
         }
         else if(state == PlayerState::climbing)
         {
             if(currentClimbingSpeed > 0) {
-                sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimationWithY("climbing up");
+                player->getAnimateBody()->setActiveAnimationWithY("climbing up");
                 if (currentClimbingSpeed == 0.1f) {
-                    sm->getActiveScene()->findByName("player")->getAnimateBody()->changeAnimationSpeed("climbing up", 0.8f);
+                    player->getAnimateBody()->changeAnimationSpeed("climbing up", 0.8f);
                 }
                 else {
-                    sm->getActiveScene()->findByName("player")->getAnimateBody()->changeAnimationSpeed("climbing up", 1.3f);
+                    player->getAnimateBody()->changeAnimationSpeed("climbing up", 1.3f);
                 }
             }
             else if (currentClimbingSpeed < 0) {
-                sm->getActiveScene()->findByName("player")->getAnimateBody()->setActiveAnimationWithY("climbing down");
-                if (groundPosition + 0.05 > sm->getActiveScene()->findByName("player")->getTransform()->getLocalPosition().y) {
+                player->getAnimateBody()->setActiveAnimationWithY("climbing down");
+                if (groundPosition + 0.05 > player->getTransform()->getLocalPosition().y) {
                     state = PlayerState::walking;
                 }
             }
 
-            float climbingY = abs(currentClimbingSpeed) * sm->getActiveScene()->findByName("player")->getTransform()->getLocalScale().y * sm->getActiveScene()->findByName("player")->getAnimateBody()->getPosition().y;
-            sm->getActiveScene()->findByName("player")->velocity = glm::vec3(0.f, climbingY, 0.f) / deltaTime;
+            float climbingY = abs(currentClimbingSpeed) * player->getTransform()->getLocalScale().y * player->getAnimateBody()->getPosition().y;
+            player->velocity = glm::vec3(0.f, climbingY, 0.f) / deltaTime;
         }
         else if (state == PlayerState::treeAttack) {
             //zaatakuj tego który jest najblizej
-            sm->getActiveScene()->findByName("player")->velocity = glm::vec3(0.f, -1.f, 0.f) / deltaTime;
-            if (groundPosition + 0.05 > sm->getActiveScene()->findByName("player")->getTransform()->getLocalPosition().y) {
+            player->velocity = glm::vec3(0.f, -1.f, 0.f) / deltaTime;
+            if (groundPosition + 0.05 > player->getTransform()->getLocalPosition().y) {
                 state = PlayerState::walking;
             }
         }
